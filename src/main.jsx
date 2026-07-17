aa578e89390428316b536e99d657a143e5be0730
0Anxiety <akpanikavictor2@gmail.com>
2026-07-17 09:01:58 +0100

    Import theme.css in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import AppRouter from './Router';
 import './index.css';
+import './theme.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
